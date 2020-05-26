207d4b17cf16fd5f4086c0da25d34f809e439aff
kdergachev <dergachev@hotmail.com>
2020-05-26 15:08:18 +0300

    transform to CRA template

---
+++ template/src/index.tsx
@@ -0,0 +1,30 @@
+/*
+ * Tesler UI CRA template (typescript)
+ * Copyright (C) 2020 Tesler Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from '@tesler-ui/core'
+import {LocaleProvider} from 'antd'
+import enUs from 'antd/es/locale-provider/en_US'
+import './index.css'
+
+const App = <Provider useEpics={false}>
+    <LocaleProvider locale={enUs}>
+        <div>Hello World</div>
+    </LocaleProvider>
+</Provider>
+
+render(App, document.getElementById('root'))
